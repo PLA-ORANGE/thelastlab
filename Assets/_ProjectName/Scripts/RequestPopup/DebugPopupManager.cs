2139a53829c002e26fa72bf0755f22569fb88b95
Teo Diaz <diaz.teo@outlook.com>
2019-10-14 12:34:13 +0200

    add Popup

---
+++ Assets/_ProjectName/Scripts/RequestPopup/DebugPopupManager.cs
@@ -0,0 +1,72 @@
+﻿///-----------------------------------------------------------------
+/// Author : Teo Diaz
+/// Date : 14/10/2019
+///-----------------------------------------------------------------
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.Github.PLAORANGE.Thelastlab.Popup
+{
+	public class DebugPopupManager : MonoBehaviour {
+		private static DebugPopupManager instance;
+		public static DebugPopupManager Instance { get { return instance; } }
+
+        [SerializeField]
+        protected Button popupAppearButton;
+        [SerializeField]
+        protected Button popupDisapearButton;
+        [SerializeField]
+        protected Button Event1Button;
+        [SerializeField]
+        protected Button Event2Button;
+
+		private void Awake(){
+			if (instance){
+				Destroy(gameObject);
+				return;
+			}
+			
+			instance = this;
+		}
+		
+		private void Start () {
+            popupAppearButton.onClick.AddListener(OnClickPopupAppearButton);
+            popupDisapearButton.onClick.AddListener(OnClickDisapearButton);
+            Event1Button.onClick.AddListener(OnClickEvent1);
+            popupAppearButton.onClick.AddListener(OnClickEvent2);
+
+        }
+
+        
+
+        protected void OnClickPopupAppearButton()
+        {
+
+        }
+
+        protected void OnClickDisapearButton()
+        {
+
+        }
+
+        protected void OnClickEvent1()
+        {
+
+        }
+        private void OnClickEvent2()
+        {
+
+        }
+
+
+        private void Update () {
+			
+		}
+		
+		private void OnDestroy(){
+			if (this == instance) instance = null;
+		}
+	}
+}
---
+++ Assets/_ProjectName/Scripts/RequestPopup/RequestPopup.cs
@@ -0,0 +1,88 @@
+﻿///-----------------------------------------------------------------
+/// Author : Teo Diaz
+/// Date : 14/10/2019
+///-----------------------------------------------------------------
+
+using UnityEngine;
+using TMPro;
+
+namespace Com.Github.PLAORANGE.Thelastlab.Popup
+{
+	public class RequestPopup : MonoBehaviour {
+
+        [SerializeField]
+        protected TextMeshProUGUI popupTitle;
+        [SerializeField]
+        protected TextMeshProUGUI popupText;
+
+        protected Vector2 initialPos;
+        protected RectTransform rectTransform;
+
+        [SerializeField]
+        protected string titlePopup;
+        [SerializeField]
+        protected string textPopup;
+
+        public string TextPopup
+        {
+            get => textPopup; 
+            set
+            {
+                textPopup = value;
+                
+            }
+        }
+
+        protected string TitlePopup
+        {
+            get => titlePopup; 
+            set
+            {
+                titlePopup = value;
+            }
+        }
+
+        protected void SetText()
+        {
+            TitlePopup = "";
+            TextPopup = "";
+
+        }
+
+        protected void SetText(string title, string text)
+        {
+            TitlePopup = title;
+            TextPopup = text;
+        }
+
+        protected void SetText(string text)
+        {
+            TitlePopup = "";
+            TextPopup = text;
+        }
+        
+
+        protected void UpdateText()
+        {
+            popupTitle.text = TitlePopup;
+            popupText.text = TextPopup;
+        }
+
+        private void Start()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            initialPos = rectTransform.anchoredPosition;
+            UpdateText();
+        }
+
+        public void FalseAnswer()
+        {
+            Debug.Log("La reponse donnee a la requete est incorrecte");
+        }
+
+        public void CorrectAnswer()
+        {
+            Debug.Log("La reponse donnee a la requete est correct");
+        }
+    }
+}
