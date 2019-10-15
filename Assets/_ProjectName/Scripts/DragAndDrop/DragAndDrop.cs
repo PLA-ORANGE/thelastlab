1d16957f346a29ec0dd6f3f6adc19f2ad3c92e50
Hugo Teyssier <h.teyssier@student.isartdigital.com>
2019-10-15 17:08:11 +0200

    deck dynamic

--- Assets/_ProjectName/Scripts/DragAndDrop/DragAndDrop.cs
+++ Assets/_ProjectName/Scripts/DragAndDrop/DragAndDrop.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Com.Github.PLAORANGE.Thelastlab {
-    public class DragAndDrop : MonoBehaviour,IDragHandler{
+    public class DragAndDrop : MonoBehaviour, IDragHandler{
         private Vector3 vector = new Vector3();
         private Camera camera;
        
@@ -20,28 +20,46 @@ namespace Com.Github.PLAORANGE.Thelastlab {
             camera = Camera.main;
         }
 
+        Transform target;
+        Card currentCardTaken;
+
         private void Update() {
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction);
 
+            // etape 1 : MouseButtonDown 
+            // Récupérer l'élément qui a été touché, le stocker dans une variable
+            // Stocker l'offset entre la position de la souris et la position de l'élement
 
-            if (hitInfo.collider != null && Input.GetMouseButton(0)) {
+            // etape 2 : 
+            // si la variable n'est pas null
+            // Placer l'élément stocker dans la variable en fonction de la position de la souris
 
-                if(hitInfo.collider.CompareTag("carte")) {
+            // etape 3 : MouseButtonUp
+            // affecter null a la variable
 
-                    if(Input.GetMouseButtonDown(0)) {
 
-                        hitInfo.collider.GetComponent<Card>().StartDrag(ray);
-                        vector.Set(ray.GetPoint(1f).x - hitInfo.transform.position.x, ray.GetPoint(1f).y - hitInfo.transform.position.y,0);
-                    }
+            if (Input.GetMouseButtonDown(0) && hitInfo)
+            {
+                currentCardTaken = hitInfo.collider.GetComponent<Card>();
+                currentCardTaken.StartDrag();
 
-                    hitInfo.transform.position = (ray.GetPoint(1f) - vector);
-                }
-                   
-                else Debug.Log(hitInfo.collider);
-
+                target = hitInfo.transform;
+                vector.Set(ray.GetPoint(1f).x - target.position.x, ray.GetPoint(1f).y - target.position.y, 0);
             }
+
+            if (target)
+            {
+                target.position = ray.GetPoint(1f) - vector;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                currentCardTaken.StopDrag();
+                target = null;
+            }
+                
 
         }
 
