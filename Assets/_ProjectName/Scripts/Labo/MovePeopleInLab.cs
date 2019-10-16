ea068f18f72fb7cc718d5e87b132a32fdbc4d142
ClementVieilly <c.vieilly@student.isartdigital.com>
2019-10-16 14:55:17 +0200

    Script pour la team dans le lab

---
+++ Assets/_ProjectName/Scripts/Labo/MovePeopleInLab.cs
@@ -0,0 +1,25 @@
+﻿///-----------------------------------------------------------------
+/// Author : #DEVELOPER_NAME#
+/// Date : #DATE#
+///-----------------------------------------------------------------
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace Com.Github.PLAORANGE.Thelastlab.Labo {
+    public class MovePeopleInLab : MonoBehaviour {
+
+        private void Start() {
+
+        }
+
+
+        private void Update () {
+           
+		}
+
+
+
+	}
+}
