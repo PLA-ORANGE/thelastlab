77b1f07f71a45bce6dead56f0621ce347186487d
ClementVieilly <c.vieilly@student.isartdigital.com>
2019-10-28 17:40:44 +0100

    SelectionInvention semi finit Manque le bouton

---
+++ Assets/_ProjectName/Scripts/Popup/InventionPopup.cs
@@ -0,0 +1,68 @@
+﻿///-----------------------------------------------------------------
+/// Author : #DEVELOPER_NAME#
+/// Date : #DATE#
+///-----------------------------------------------------------------
+
+using Pixelplacement;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Com.Github.PLAORANGE.Thelastlab.Popup
+{
+    public class InventionPopup : Popup
+    {
+        public bool onClick = false;
+        protected PopupInventionContainer container;
+        [SerializeField] RectTransform inventionPos;
+        [SerializeField] private GameObject invention;
+        [SerializeField] private RectTransform centerScreen;
+        [SerializeField] private Button button;
+        private RectTransform inventionRect;
+        private Vector2 initalPosition;
+        
+        
+        public override void Start() {
+            base.Start();
+            rectTransform.localScale = new Vector2(1, 1);
+            Appear();
+            container = FindObjectOfType<PopupInventionContainer>();
+            invention = GameObject.Instantiate(invention, inventionPos);
+            inventionRect = invention.AddComponent<RectTransform>(); 
+            inventionRect.localPosition = Vector3.zero;
+            invention.GetComponent<Invention>().win = false;
+            SetText(name);
+           
+
+        }
+
+
+        public override void Appear() {
+            //Tween.LocalScale(rectTransform, new Vector2(0.5f, 0.6f), 1f, 0.1f, Tween.EaseOutBack);
+            exist = true;
+        }
+        
+        public void OnClick() {
+            if(!onClick) {
+                initalPosition = transform.position;
+                container.CheckPopup();
+                SetText(""); 
+                Tween.LocalScale(rectTransform, new Vector2(2.5f, 2.5f), 0.3f, 0, Tween.EaseOutBack);
+                Tween.Position(rectTransform, new Vector2(centerScreen.position.x, centerScreen.position.y), 0.5f, 0, Tween.EaseInOutBack,Tween.LoopType.None,null, ()=> { SetText("inserer description du bail tu connais le delire boy"); });
+                onClick = true;
+                GameObject.Instantiate(button,transform);
+                
+               
+            }
+           
+            
+        }
+
+        public void NotOnClick() {
+            onClick = false;
+            SetText(""); 
+            Tween.LocalScale(rectTransform, new Vector2(1, 1), 0.3f, 0, Tween.EaseOutBack);
+            Tween.Position(rectTransform, initalPosition, 0.5f, 0, Tween.EaseInOutBack, Tween.LoopType.None, null, () => { SetText(name); });
+        }
+    }
+}
---
+++ Assets/_ProjectName/Scripts/Popup/PopupInventionContainer.cs
@@ -0,0 +1,30 @@
+﻿///-----------------------------------------------------------------
+/// Author : #DEVELOPER_NAME#
+/// Date : #DATE#
+///-----------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Github.PLAORANGE.Thelastlab.Popup
+{
+	public class PopupInventionContainer : MonoBehaviour {
+
+        [SerializeField] protected InventionPopup invention1; 
+        [SerializeField] protected InventionPopup invention2; 
+        [SerializeField] protected InventionPopup invention3;
+
+        protected List<InventionPopup> inventionPopupList = new List<InventionPopup>(); 
+		private void Start () {
+            inventionPopupList.Add(invention1); 
+            inventionPopupList.Add(invention2); 
+            inventionPopupList.Add(invention3); 
+		}
+
+        public void CheckPopup() {
+            for(int i = 0; i < inventionPopupList.Count; i++) {
+                if(inventionPopupList[i].onClick) inventionPopupList[i].NotOnClick(); 
+            }
+        }
+	}
+}
