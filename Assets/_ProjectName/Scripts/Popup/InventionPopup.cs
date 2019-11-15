fdc523ac796633becc297b7f9b91018a3c760b8d
ClementVieilly <c.vieilly@student.isartdigital.com>
2019-11-15 11:43:21 +0100

    Fix Popup Choix du projet

--- Assets/_ProjectName/Scripts/Popup/InventionPopup.cs
+++ Assets/_ProjectName/Scripts/Popup/InventionPopup.cs
@@ -11,26 +11,27 @@ namespace Com.Github.PLAORANGE.Thelastlab.Popup
 {
     public class InventionPopup : Popup
     {
-        public bool onClick = false;
+        [HideInInspector]public bool onClick = false;
         protected PopupInventionContainer container;
         [SerializeField] RectTransform inventionPos;
         [SerializeField] private GameObject invention;
         [SerializeField] private RectTransform centerScreen;
+        [SerializeField] private bool testPopup; 
         private RectTransform inventionRect;
         private Vector2 initalPosition;
         
         
         public override void Start() {
             base.Start();
-            rectTransform.localScale = new Vector2(1, 1);
+            rectTransform.localScale = new Vector2(1.5f, 1.5f);
             Appear();
             container = FindObjectOfType<PopupInventionContainer>();
             invention = GameObject.Instantiate(invention, inventionPos);
             inventionRect = invention.AddComponent<RectTransform>(); 
             inventionRect.localPosition = Vector3.zero;
             invention.GetComponent<Invention>().win = false;
-            SetText(name);
             initalPosition = transform.position;
+
 
         }
 
@@ -40,11 +41,11 @@ namespace Com.Github.PLAORANGE.Thelastlab.Popup
             exist = true;
         }
         
-        protected void OnClick() {
+        public void OnClick() {
             container.CheckPopup();
             SetText("");
-            Tween.LocalScale(rectTransform, new Vector2(2.5f, 2.5f), 0.3f, 0, Tween.EaseOutBack);
-            Tween.Position(rectTransform, new Vector2(centerScreen.position.x, centerScreen.position.y), 0.5f, 0, Tween.EaseInOutBack, Tween.LoopType.None, null, () => { SetText("inserer description du bail tu connais le delire boy"); });
+            Tween.LocalScale(rectTransform, new Vector2(2.2f, 2.2f), 0.3f, 0, Tween.EaseOutBack);
+            Tween.Position(rectTransform, new Vector2(centerScreen.position.x, centerScreen.position.y), 0.5f, 0, Tween.EaseInOutBack, Tween.LoopType.None, null, SetDescription);
             onClick = true;
             container.CheckWichInvention(gameObject.name);
 
@@ -54,7 +55,23 @@ namespace Com.Github.PLAORANGE.Thelastlab.Popup
             onClick = false;
             SetText(""); 
             Tween.LocalScale(rectTransform, new Vector2(1, 1), 0.3f, 0, Tween.EaseOutBack);
-            Tween.Position(rectTransform, initalPosition, 0.5f, 0, Tween.EaseInOutBack, Tween.LoopType.None, null, () => { SetText(name); });
+            Tween.Position(rectTransform, initalPosition, 0.5f, 0, Tween.EaseInOutBack, Tween.LoopType.None, null, SetDescription);
+        }
+
+        private void SetDescription() {
+            if(testPopup && onClick) {
+                SetText("Séticyksapu était une ville prospère jusqu’à ce que des gaz toxiques y soient détectés, et que la population ait dû être évacuée.");
+                popupText.fontSize = 15;
+                popupText.rectTransform.anchoredPosition = new Vector2(popupText.rectTransform.anchoredPosition.x, -17); 
+            }
+            else if(testPopup && !onClick) {
+                SetText("Drone assainissant");
+                popupText.fontSize = 20;
+                popupText.rectTransform.anchoredPosition = new Vector2(popupText.rectTransform.anchoredPosition.x, -1.5f);
+            }
+
+            if(!testPopup) SetText("Bloqué"); 
+
         }
     }
 }
